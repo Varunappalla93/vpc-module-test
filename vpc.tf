2e0a3d2a0a88ca17ac7934d814676efb6e928f3c
Varunappalla1993 <an.varun1993@gmail.com>
2026-03-10 13:28:00 +0530

    vpc

--- vpc.tf
+++ vpc.tf
@@ -4,11 +4,17 @@ module "vpc" {
   source      = "git::https://github.com/Varunappalla93/Terrform-AWS-VPC.git?ref=main" # wil take from git repo
   project     = var.project
   environment = var.env
-  is_peering_required=true
-  
+
+  vpc_cidr = "10.0.0.0/16"
+
+  public_subnet_cidrs   = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs  = ["10.0.11.0/24", "10.0.12.0/24"]
+  database_subnet_cidrs = ["10.0.21.0/24", "10.0.22.0/24"]
+
+
+  is_peering_required = true
+
 }
-
-
 
 # terraform init
 # terraform plan
